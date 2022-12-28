13550d3f4d8cc5cbc07219427a58b57365985a70
fasc786gm <fasc786gmail.com>
2022-12-28 20:45:00 +0530

    new file:   src/app/accordion/accordion.component.css 	new file:   src/app/accordion/accordion.component.html 	new file:   src/app/accordion/accordion.component.spec.ts 	new file:   src/app/accordion/accordion.component.ts 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	modified:   src/app/tab/tab.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccordionComponent } from './accordion/accordion.component';
+import { TabComponent } from './tab/tab.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'tab',
+    component:TabComponent
+  },
+  {
+    path:'accordion',
+    component:AccordionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabComponent } from './tab/tab.component';
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabComponent
+    TabComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule,
