de156ab74de373cbfaff956d3133362357d98d9b
fasc786gm <fasc786gmail.com>
2022-12-28 20:49:40 +0530

    modified:   src/app/accordion/accordion.component.html 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/header/header.component.css 	new file:   src/app/header/header.component.html 	new file:   src/app/header/header.component.spec.ts 	new file:   src/app/header/header.component.ts 	modified:   src/app/tab/tab.component.css 	modified:   src/app/tab/tab.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabComponent } from './tab/tab.component';
 import { AccordionComponent } from './accordion/accordion.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabComponent,
-    AccordionComponent
+    AccordionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
